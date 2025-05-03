f905bc25f6f6603bbf6cefc3190334bf818b7262
Swanson011 <joshua.swanson27@houghton.edu>
2025-05-02 22:47:12 -0400

    f

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 function Footer() {
   return (
-    <footer className="App-footer">
+    <footer className="App-footer" style={{ padding: '1px 0', position: 'relative' }}>
       <p>&copy; 2025 SolidBase Construction. All rights reserved.</p>
       <a href="/contact">Contact Us</a>
     </footer>

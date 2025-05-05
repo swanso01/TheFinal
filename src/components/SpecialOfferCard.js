a381f78a6adaa0efef79bf2e01555562eae71c8b
Swanson011 <joshua.swanson27@houghton.edu>
2025-05-05 14:39:37 -0400

    Major update: new changes

--- src/components/SpecialOfferCard.js
+++ src/components/SpecialOfferCard.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
 import '../App.css';
 
 function SpecialOfferCard({ offer }) {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleAddToCart = () => {
+    if (!user) {
+      alert('Please log in to add items to the cart.');
+      return;
+    }
+
+    const newCartItem = {
+      name: offer.title,
+      quantity: 1,
+    };
+
+    setUser({
+      ...user,
+      selectedItems: [...(user.selectedItems || []), newCartItem],
+    });
+
+    alert(`${offer.title} has been added to your cart!`);
+  };
+
   return (
     <div className="App-special-offer-card">
       <h4>{offer.title}</h4>
       <p>{offer.description}</p>
       <p>Discount: {offer.discount}</p>
-      <button className="App-special-offer-button">View Offer</button>
+      <button className="App-special-offer-button" onClick={handleAddToCart}>
+        Add to Cart
+      </button>
     </div>
   );
 }
